8d1be7b1a540b88581faa137ee949b91fea17572
highwide <hochweit728@gmail.com>
2017-05-29 01:44:10 +0900

    modify users implements

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :password, length: { minimum: 3 }, if: :new_or_changing_password?
+  validates :password, confirmation: true, if: :new_or_changing_password?
+  validates :password_confirmation, presence: true, if: :new_or_changing_password?
+
+  validates :email, uniqueness: true
+
+  private
+
+  def new_or_changing_password?
+    new_record? || changes[:crypted_password]
+  end
 end
