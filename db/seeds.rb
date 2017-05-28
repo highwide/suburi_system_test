b011697f3ee446db9f51ea706035a3a184c17f0e
highwide <hochweit728@gmail.com>
2017-05-29 03:18:56 +0900

    Add user data for test/dev

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ActiveRecord::Base.transaction do
+  # enable to login with test@example.com/hogehoge
+  user = User.find_or_initialize_by(
+    email: 'test@example.com',
+    crypted_password: '$2a$10$Qf8isb9NbJgWstWepbvZiOXXRgE8nvZVJUH51fwQtjQM44/t0.wyO',
+    salt: 'RbvkyAJsHr-DczsuVnDD'
+  )
+  user.save!(validate: false)
+end
